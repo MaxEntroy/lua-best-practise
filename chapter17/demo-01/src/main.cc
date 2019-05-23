8468fdd6ecfbc0f60b3ccc8b92099a2666e8002a
MaxEntroy <likang@tju.edu.cn>
2019-05-23 11:12:28 +0800

    chapter17: init demo-01

---
+++ chapter17/demo-01/src/main.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+extern "C" {
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
+
+int main(void) {
+    // gflags传下脚本名称
+    // SetRoot设置下
+    return 0;
+}
