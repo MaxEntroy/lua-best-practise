b77c441c741a1e266fa19ceef2482c9b461f25d2
MaxEntroy <likang@tju.edu.cn>
2019-08-07 14:24:48 +0800

    Finish demo-03

--- chapter20/demo-03/luafile/test.lua
+++ chapter20/demo-03/luafile/test.lua
@@ -3,3 +3,13 @@ print("hello,lua")
 function AddAndMinus(x, y)
     return x + y, x - y
 end
+
+
+cal = {}
+function cal:Add(x, y)
+    return x + y
+end
+
+function cal:Minus(x, y)
+    return x - y
+end
--- chapter20/demo-03/src/main.cc
+++ chapter20/demo-03/src/main.cc
@@ -33,12 +33,42 @@ void Test(lua_State* L, const std::string& script) {
     lua_settop(L, 0);
 }
 
+void Test1(lua_State* L, const std::string& script) {
+    luaL_dofile(L, script.c_str());
+
+    int x = 3;
+    int y = 4;
+
+    lua_getglobal(L, "cal");
+
+    lua_getfield(L, -1, "Add");
+    lua_getglobal(L, "cal"); -- 写法非常trick，结合lua脚本来看
+    lua_pushinteger(L, x);
+    lua_pushinteger(L, y);
+    lua_pcall(L, 3, 1, 0);
+    int ret = lua_tonumber(L, -1);
+    lua_pop(L, 1);
+    std::cout << "Add = " << ret << std::endl;
+
+    lua_getfield(L, -1, "Minus");
+    lua_getglobal(L, "cal");
+    lua_pushinteger(L, x);
+    lua_pushinteger(L, y);
+    lua_pcall(L, 3, 1, 0);
+    ret = lua_tonumber(L, -1);
+    lua_pop(L, 1);
+    std::cout << "Minus = " << ret << std::endl;
+
+    lua_settop(L, 0);
+}
+
 int main(int argc, char* argv[]) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
 
-    Test(L, FLAGS_script_path);
+    //Test(L, FLAGS_script_path);
+    Test1(L, FLAGS_script_path);
 
     lua_close(L);
     return 0;
