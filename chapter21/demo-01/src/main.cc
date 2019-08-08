47c38a69ce99e0f8c2118a83848ea582ce6815e7
MaxEntroy <likang@tju.edu.cn>
2019-08-08 10:45:34 +0800

    chapter21: Add demo-01

---
+++ chapter21/demo-01/src/main.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+extern "C" {
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
+
+#include <gflags/gflags.h>
+
+DEFINE_string(init_path, "", "lua init script");
+DEFINE_string(script_path, "", "the lua script path" );
+
+void Test(lua_State*& L, const std::string& lua_script) {
+    luaL_dofile(L, lua_script.c_str());
+
+    lua_getglobal(L, "SetScriptPath");
+    lua_pushstring(L, FLAGS_script_path.c_str());
+    lua_pcall(L, 1, 0, 0);
+
+    std::string msg = "hello, world";
+    lua_getglobal(L, "DoTask");
+    lua_pushstring(L, msg.c_str());
+    lua_pcall(L, 1, 1, 0);
+
+    std::string ret = lua_tostring(L, -1);
+    std::cout << ret << std::endl;
+
+    lua_settop(L, 0);
+}
+
+int main(int argc, char* argv[]) {
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
+
+    Test(L, FLAGS_init_path);
+
+    lua_close(L);
+    return 0;
+}
