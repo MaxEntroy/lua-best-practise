353626788713306159a02246e841698a8047b702
MaxEntroy <likang@tju.edu.cn>
2019-08-12 21:21:55 +0800

    chapter21: Add README

--- chapter21/demo-01/src/main.cc
+++ chapter21/demo-01/src/main.cc
@@ -13,16 +13,16 @@ extern "C" {
 DEFINE_string(init_path, "", "lua init script");
 DEFINE_string(script_path, "", "the lua script path" );
 
-void Test(lua_State*& L, const std::string& lua_script) {
+void Driver(lua_State*& L, const std::string& lua_script) {
     luaL_dofile(L, lua_script.c_str());
 
     lua_getglobal(L, "SetScriptPath");
     lua_pushstring(L, FLAGS_script_path.c_str());
     lua_pcall(L, 1, 0, 0);
 
-    std::string msg = "hello, world";
+    std::string req_arg = "hello, world";
     lua_getglobal(L, "DoTask");
-    lua_pushstring(L, msg.c_str());
+    lua_pushstring(L, req_arg.c_str());
     lua_pcall(L, 1, 1, 0);
 
     std::string ret = lua_tostring(L, -1);
@@ -36,7 +36,7 @@ int main(int argc, char* argv[]) {
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
 
-    Test(L, FLAGS_init_path);
+    Driver(L, FLAGS_init_path);
 
     lua_close(L);
     return 0;
