0ea2a2a95138f33f77ad86c5aa853261b9bef9df
MaxEntroy <likang@tju.edu.cn>
2019-09-18 10:59:24 +0800

    chapter21: Add demo-08.

---
+++ chapter21/demo-08/thirdparty/Other/src/other_lib.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+
+extern "C" {
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
+
+// --------------------------------------------------------------
+// Other lib
+// --------------------------------------------------------------
+static int OtherFoo(lua_State* L) {
+    std::cout << "hello, world!" << std::endl;
+    return 0;
+}
+
+static int OtherSumAndAver(lua_State* L) {
+    int n = lua_gettop(L);
+    lua_Number sum = 0.0;
+    for(int i = 1; i <= n; ++i) {
+        if(lua_isnumber(L, i)) {
+            lua_Number x = lua_tonumber(L, i);
+            sum += x;
+        }
+        else {
+            lua_pushliteral(L, "Invalid arguments!");
+            lua_error(L);
+        }
+    }
+    lua_Number avg = sum / n;
+    lua_pushnumber(L, sum);
+    lua_pushnumber(L, avg);
+    return 2;
+}
+
+static const luaL_Reg otherlib[] = {
+    {"Foo", OtherFoo},
+    {"SumAndAver", OtherSumAndAver},
+    {NULL, NULL}
+};
+
+int luaopen_other(lua_State* L) {
+    luaL_newlib(L, otherlib);
+    return 1;
+};
